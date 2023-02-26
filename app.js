f2003b7c773d80ebbbe4ab3fc934100550c5b17e
dejunqi <dejunqi2008@gmail.com>
2023-02-26 11:58:42 -0800

    login function implementation

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const session = require('express-session')
+
 
 const blogRouter = require('./routes/blog');
 const userRouter = require('./routes/user');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json()); // requestion information are converted to json into body
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'jwo$%#$Apq',
+    cookie: {
+        path: '/', // default
+        httpOnly: true, // default
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}))
 
 
 app.use('/api/blog', blogRouter);
@@ -22,18 +32,18 @@ app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const { login } = require('../controller/user');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
 const router = express.Router();
 
 router.post('/login', (req, res, next) => {
     const { username, password } = req.body;
-    res.json({
-        errno: 0,
-        data: {username, password}
-    })
+    const result = login(username, password);
+    
+    return result.then(data => {
+        if (data.username) {
+            req.session.username = data.username;
+            req.session.realname = data.realname;
+            return res.json(new SuccessModel());
+        }
+        res.json(new ErrorModel('Login failed'));
+    });
 });
 
 module.exports = router;
