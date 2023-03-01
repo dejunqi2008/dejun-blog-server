15805879be3e46ffea70923552006af14104dc8d
dejunqi <dejunqi2008@gmail.com>
2023-02-28 19:38:59 -0800

    use connection pool instead of single connect

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -2,12 +2,16 @@ const mysql = require('mysql')
 const { MYSQL_CONF } = require('../config/db')
 
 const con = mysql.createConnection(MYSQL_CONF)
+const pool = mysql.createPool({
+    ...MYSQL_CONF,
+    connectionLimit: 10
+})
 
-con.connect()
+// con.connect()
 
 function exec(sql) {
     const promise = new Promise((resolve, reject) => {
-        con.query(sql, (err, result) => {
+        pool.query(sql, (err, result) => {
             if (err) {
                 reject(err)
                 return
