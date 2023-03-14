7fccc12f6fda34393a0e121b2e0b60403ee00368
dejunqi <dejunqi2008@gmail.com>
2023-03-14 15:31:33 -0700

    image upload

--- src/routes/image.js
+++ src/routes/image.js
@@ -1,34 +1,37 @@
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
-const { uploadFile } = require('../utils/s3');
+const { s3MethodFactor } = require('../utils/s3');
+const fs = require('fs');
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+const { execSync } = require("child_process");
 
 
 const express = require('express');
-const { SuccessModel } = require('../model/resModel');
+const { SuccessModel, ErrorModel } = require('../model/resModel');
 const router = express.Router();
 
-// this api upload a image to server
-router.put('/add', upload.single('image'), (req, res, next) => {
-    if (!!req.file) {
-        return res.json(new SuccessModel({
-            message: 'Successully put the image to remote server, waiting to be upload.'
-        }))
-    } else {
-        return res.json(new Error("Failed to deliver the image to the server."))
+
+router.post('/new', upload.single('image'), async (req, res, next) => {
+    try {
+        const result = await s3MethodFactor('uploadFile')(req.file);
+        const { $metadata: { httpStatusCode }} = result;
+        if (httpStatusCode === 200) {
+            await unlinkFile(req.file.path);
+            const { Key } = result;
+            return res.json(new SuccessModel({imagePath: `/images/${Key}`}));
+        } else {
+            res.json(new ErrorModel());
+        }
+    } catch(err) {
+        res.json(err);
     }
 })
 
-router.post('/new', async (req, res, next) => {
-    console.log(req.file);
-    try {
-        const result = await uploadFile(req.file);
-        console.log('upload call result', result);
-        return res.json(new SuccessModel(result));
-    } catch(err) {
-        console.log(err)
-        res.json(err);
-    }
-    
+router.get('/:key', async (req, res, next) => {
+    const key = req.params.key;
+    const response = await s3MethodFactor('getFileStream')(key);
+    response.Body.pipe(res);
 })
 
 
