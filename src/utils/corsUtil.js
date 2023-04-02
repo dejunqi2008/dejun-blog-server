e7986ec8b418f0b22147c1aa3e562f07a60e5fcc
dejunqi <dejunqi2008@gmail.com>
2023-04-01 20:09:13 -0700

    add new domain for CORS whitelist v3

--- src/utils/corsUtil.js
+++ src/utils/corsUtil.js
@@ -1,7 +1,7 @@
 const whitelistDomains = [
     // 'https://dejun-blog-client.herokuapp.com',
     'http://localhost:3000',
-    'https://www.dejun-cms.com/'
+    'https://www.dejun-cms.com'
 ];
 
 const whitelistMethods = ['GET','POST','DELETE','UPDATE','PUT','PATCH'];
