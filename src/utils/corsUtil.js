74ccb73cbdc91cfd2f0a6bb66e8a63a6df11d989
dejunqi <dejunqi2008@gmail.com>
2023-04-02 13:36:23 -0700

    test deploy

--- src/utils/corsUtil.js
+++ src/utils/corsUtil.js
@@ -1,7 +1,9 @@
 const whitelistDomains = [
     // 'https://dejun-blog-client.herokuapp.com',
     'http://localhost:3000',
-    'https://www.dejun-cms.com'
+    'https://www.dejun-cms.com',
+    'https://test-deploy.d1ut9q9f8s2667.amplifyapp.com',
+    'https://mainline.d1ut9q9f8s2667.amplifyapp.com'
 ];
 
 const whitelistMethods = ['GET','POST','DELETE','UPDATE','PUT','PATCH'];
